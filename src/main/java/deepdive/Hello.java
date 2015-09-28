22fa4501a09562b2784b7b1d9dc40470e8003ab2
Igor Petrov <petrov.igor.od@gmail.com>
2015-09-28 17:19:18 +0300

    Initial commit

---
+++ src/main/java/deepdive/Hello.java
@@ -0,0 +1,11 @@
+package deepdive;
+
+/**
+ * Created by igp on 9/28/15.
+ */
+public class Hello {
+
+    public static void main(String ... args) {
+        System.out.println("Hello");
+    }
+}
---
+++ src/test/java/deepdive/HelloTest.java
@@ -0,0 +1,12 @@
+package deepdive;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class HelloTest {
+
+    @Test
+    public void testAssertTrue() {
+        Assert.assertEquals(1, 1);
+    }
+}
